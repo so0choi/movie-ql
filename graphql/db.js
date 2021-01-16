29670a6bb7e0f2d43d956bc29263eca681ac15e6
so0choi <simc2644@gmail.com>
2021-01-16 22:10:57 +0900

    Add more apis - movie detail, movie suggestions

--- graphql/db.js
+++ graphql/db.js
@@ -1,48 +1,50 @@
 export let movies = [
-    {
-        id: "1",
-        name: "Star Wars",
-        score: 4
-    },
-    {
-        id: "2",
-        name: "Avengers",
-        score: 5
-    },
-    {
-        id: "3",
-        name: "Spiderman",
-        score: 5
-    },
-    {
-        id: "4",
-        name: "Logan",
-        score: 2
-    }
+  {
+    id: "1",
+    name: "Star Wars",
+    score: 4,
+  },
+  {
+    id: "2",
+    name: "Avengers",
+    score: 5,
+  },
+  {
+    id: "3",
+    name: "Spiderman",
+    score: 5,
+  },
+  {
+    id: "4",
+    name: "Logan",
+    score: 2,
+  },
 ];
 
 export const getMovies = () => movies;
 
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => String(id) === movie.id);
-    return filteredMovies[0];
-}
+export const getById = (id) => {
+  const filteredMovies = movies.filter((movie) => String(id) === movie.id);
+  return filteredMovies[0];
+};
 
 export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
-}
+  const cleanedMovies = movies.filter((movie) => movie.id !== String(id));
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
 
 export const addMovie = (name, score) => {
-    const newMovie = { 
-        id: `${movies.length + 1}`,
-        name, score};
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
 
-        movies.push(newMovie);
-        return newMovie;
-}
+  movies.push(newMovie);
+  return newMovie;
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,24 +1,25 @@
-
 // import { getMovies, getById, addMovie, deleteMovie } from './db.js';
 
-import {getMovies} from './yts.js';
+import { getMovies, getMovie, getSuggestions } from "./yts.js";
 
 const resolvers = {
-    //db.js
-    // Query: {
-    //     movies: () => getMovies(),
-    //     movie: (_, { id }) =>
-    //         getById(id)
-    // },
-    // Mutation: {
-    //     addMovie: (_, {name, score}) => addMovie(name, score),
-    //     deleteMovie: (_,  {id}) => deleteMovie(id)
-    // }
-    //yts.js
+  //db.js
+  // Query: {
+  //     movies: () => getMovies(),
+  //     movie: (_, { id }) =>
+  //         getById(id)
+  // },
+  // Mutation: {
+  //     addMovie: (_, {name, score}) => addMovie(name, score),
+  //     deleteMovie: (_,  {id}) => deleteMovie(id)
+  // }
+  //yts.js
 
-    Query : {
-        movies: (_, {limit, rating}) => getMovies(limit, rating)
-    }
-}
+  Query: {
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
+  },
+};
 
 export default resolvers;
