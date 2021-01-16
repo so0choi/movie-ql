ee47a51635e7b19fc75e009959a8e77155273bd0
so0choi <simc2644@gmail.com>
2021-01-16 21:35:33 +0900

    Add yts code (wrap rest api with graphql)

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,23 @@
 
-import { getMovies, getById, addMovie, deleteMovie } from './db.js'
+// import { getMovies, getById, addMovie, deleteMovie } from './db.js';
+
+import {getMovies} from './yts.js';
 
 const resolvers = {
-    Query: {
-        movies: () => getMovies(),
-        movie: (_, { id }) =>
-            getById(id)
-    },
-    Mutation: {
-        addMovie: (_, {name, score}) => addMovie(name, score),
-        deleteMovie: (_,  {id}) => deleteMovie(id)
+    //db.js
+    // Query: {
+    //     movies: () => getMovies(),
+    //     movie: (_, { id }) =>
+    //         getById(id)
+    // },
+    // Mutation: {
+    //     addMovie: (_, {name, score}) => addMovie(name, score),
+    //     deleteMovie: (_,  {id}) => deleteMovie(id)
+    // }
+    //yts.js
+
+    Query : {
+        movies: (_, {limit, rating}) => getMovies(limit, rating)
     }
 }
 
